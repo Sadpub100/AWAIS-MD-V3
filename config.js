e0e5d5185887a7afc82271de951406aa0a15bca3
Sadpub100 <sad.pub0582@gmail.com>
2025-07-21 09:15:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
     ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M AWAIS-MD WHATSAPP BOT 😊",
     ANTI_LINK: process.env.ANTI_LINK || "false",
     ANTI_BAD: process.env.ANTI_BAD || "false",
-    PREFIX: process.env.PREFIX || "•",
+    PREFIX: process.env.PREFIX || "-",
     FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
     AUTO_REACT: process.env.AUTO_REACT || "false",
     OWNER_REACT: process.env.OWNER_REACT || "false",
